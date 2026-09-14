3adbe1f0e906dd19d696770907be822b32e42f1d
Call me Zillion <fdambock@gmail.com>
2026-09-14 14:52:11 +0200

    feat: bootstrap OG FASTR browser game v8

---
+++ character-game-v8/boot.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFExporter } from 'three/addons/exporters/GLTFExporter.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { AfterimagePass } from 'three/addons/postprocessing/AfterimagePass.js';
+Object.assign(globalThis,{THREE,OrbitControls,GLTFExporter,EffectComposer,RenderPass,UnrealBloomPass,AfterimagePass});
+function load(src){return new Promise((ok,fail)=>{const s=document.createElement('script');s.src=src;s.onload=ok;s.onerror=fail;document.body.appendChild(s);});}
+await load('../character-lab-v7/part1.js');
+await load('../character-lab-v7/part2.js');
+await load('./game.js');
