8dea623ea2b6bc34e57177f3cb6dcf237264c1aa
Call me Zillion <fdambock@gmail.com>
2026-09-14 13:52:29 +0200

    refine: add studio environment lighting support

--- character-lab-v7/boot.js
+++ character-lab-v7/boot.js
@@ -5,6 +5,7 @@ import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { AfterimagePass } from 'three/addons/postprocessing/AfterimagePass.js';
-Object.assign(globalThis,{THREE,OrbitControls,GLTFExporter,EffectComposer,RenderPass,UnrealBloomPass,AfterimagePass});
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+Object.assign(globalThis,{THREE,OrbitControls,GLTFExporter,EffectComposer,RenderPass,UnrealBloomPass,AfterimagePass,RoomEnvironment});
 function load(src){return new Promise((ok,fail)=>{const s=document.createElement('script');s.src=src;s.onload=ok;s.onerror=fail;document.body.appendChild(s);});}
 await load('./part1.js'); await load('./part2.js'); await load('./part3.js');
