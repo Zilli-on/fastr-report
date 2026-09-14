6e03a1ae3a8e072d8c1e494ca9fca67c937b3191
Call me Zillion <fdambock@gmail.com>
2026-09-14 12:02:23 +0200

    feat: add v7 module bootstrap

---
+++ character-lab-v7/boot.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFExporter } from 'three/addons/exporters/GLTFExporter.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { AfterimagePass } from 'three/addons/postprocessing/AfterimagePass.js';
+Object.assign(globalThis,{THREE,OrbitControls,GLTFExporter,EffectComposer,RenderPass,UnrealBloomPass,AfterimagePass});
+function load(src){return new Promise((ok,fail)=>{const s=document.createElement('script');s.src=src;s.onload=ok;s.onerror=fail;document.body.appendChild(s);});}
+await load('./part1.js'); await load('./part2.js'); await load('./part3.js');
